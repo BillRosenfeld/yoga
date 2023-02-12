7af39e69ac9ddcf8ee5fa3a39110b64a99299d78
Bill Rosenfeld <bill.rosenfeld@me.com>
2023-02-12 15:29:47 -0500

    working code

---
+++ src/main/java/bill/rosenfeld/yoga/Yoga.java
@@ -0,0 +1,13 @@
+package bill.rosenfeld.yoga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yoga {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Yoga.class, args);
+	}
+
+}
---
+++ src/test/java/bill/rosenfeld/yoga/YogaTests.java
@@ -0,0 +1,13 @@
+package bill.rosenfeld.yoga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YogaTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
