7a05c370afe6b85e62252be64146fc824680fe0a
Bill Rosenfeld <bill.rosenfeld@me.com>
2025-03-30 10:13:19 -0400

    sql startup

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into asana (name) values ('Adho Mukha Svanasana');
+insert into asana (name) values ('Ananda Balasana');
+insert into asana (name) values ('Virabhadrasana I');
+insert into asana (name) values ('Virabhadrasana II');
